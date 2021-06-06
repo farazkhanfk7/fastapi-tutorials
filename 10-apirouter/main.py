6ad6d8ca39c23eed8879737428acd912bfa372ff
farazkhanfk7 <farazkhan138@gmail.com>
2021-06-06 17:52:44 +0530

    10-apirouter

---
+++ 10-apirouter/main.py
@@ -0,0 +1,21 @@
+from blog import models, schemas
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from typing import List
+from sqlalchemy.orm import Session
+from blog.utils import get_db,get_hashed_pass
+from blog.database import engine
+from blog.routes import users,blogs
+import uvicorn
+
+models.Base.metadata.create_all(engine)
+
+app = FastAPI()
+app.include_router(users.router)
+app.include_router(blogs.router)
+
+@app.get('/')
+def index():
+    return {"data":"not yet"}
+
+if __name__=="__main__":
+    uvicorn.run(app,host="127.0.0.1",port=9000)
